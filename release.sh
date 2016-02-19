4abf6219dcb387523d2aeee5271120ed551db7f3
Sean Massung <massung1@illinois.edu>
2016-02-19 09:58:22 -0600

    fix path in release script

--- release.sh
+++ release.sh
@@ -12,7 +12,7 @@ if [ $? -ne 0 ]; then
 fi
 
 cp web/javascript/index.js /srv/www/timan103/html/nlp-demo/javascript/
-cp web/css/tagged-text.css /srv/www/timan103/nlp-demo/css/
+cp web/css/tagged-text.css /srv/www/timan103/html/nlp-demo/css/
 cp web/index.html /srv/www/timan103/html/nlp-demo/
 
 echo "Success! Don't forget to restart the API server if necessary."
